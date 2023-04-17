7133e4349b3127b78d79f54550804e6c8d9a8eb6
brezhoneg56 <julien.cosson56@gmail.com>
2023-04-17 17:45:05 +0200

    actually implementing different functions files and linking them to main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ Created on Wed Apr 12 09:42:28 2023
 import os
 import shutil
 import subprocess
-import fileinput
-
+#import fileinput
+from src import boundary_conditions as bc, preprocessing as pre
+#decimal_analysis, sweep_1_initialization, copytree
 ######### PATHS ########################## 
 basepath="/home/jcosson/workspace/henersj-shootingdata-3b74bb73f55e/calcs/moderate_deformed/primal/"
 calcs_undeformed="/home/jcosson/workspace/henersj-shootingdata-3b74bb73f55e/calcs/undeformed_turbulent/"
@@ -20,70 +21,7 @@ os.chdir(basepath)
 
 ############## FUNCTIONS ####################################
 
-def decimal_analysis(number):  ##analysis of how many decimals my number has : 1, 2 ou 3 décimales
-        if number * 10 % 10 == 0:
-            return round(number,2)
-        else:
-            return round(number,3)
 
-def copytree(src, dst, symlinks=False, ignore=None):
-    """
-    This is an improved version of shutil.copytree which allows writing to
-    existing folders and does not overwrite existing files but instead appends
-    a ~1 to the file name and adds it to the destination path.
-    """
-
-    names = os.listdir(src)
-    if ignore is not None:
-        ignored_names = ignore(src, names)
-    else:
-        ignored_names = set()
-
-    if not os.path.exists(dst):
-        os.makedirs(dst)
-        shutil.copystat(src, dst)
-    errors = []
-    for name in names:
-        if name in ignored_names:
-            continue
-        srcname = os.path.join(src, name)
-        dstname = os.path.join(dst, name)
-        i = 1
-        while os.path.exists(dstname) and not os.path.isdir(dstname):
-            parts = name.split('.')
-            file_name = ''
-            file_extension = parts[-1]
-            # make a new file name inserting ~1 between name and extension
-            for j in range(len(parts)-1):
-                file_name += parts[j]
-                if j < len(parts)-2:
-                    file_name += '.'
-            suffix = file_name + '~' + str(i) + '.' + file_extension
-            dstname = os.path.join(dst, suffix)
-            i+=1
-        try:
-            if symlinks and os.path.islink(srcname):
-                linkto = os.readlink(srcname)
-                os.symlink(linkto, dstname)
-            elif os.path.isdir(srcname):
-                copytree(srcname, dstname, symlinks, ignore)
-            else:
-                shutil.copy2(srcname, dstname)
-        except (IOError, os.error) as why:
-            errors.append((srcname, dstname, str(why)))
-        # catch the Error from the recursive copytree so that we can
-        # continue with other files
-        except BaseException as err:
-            errors.extend(err.args[0])
-    try:
-        shutil.copystat(src, dst)
-    except WindowsError:
-        # can't copy file access times on Windows
-        pass
-    except OSError as why:
-        errors.extend((src, dst, str(why)))
-    if errors:
-        raise BaseException(errors)
 
 def pimpleDyMFoam(folder_name, sweep_name,i):
     interval_name=myinterval.format(i)
@@ -110,7 +48,7 @@ def preparePostProcessing(folder_name, sweep_name):
         list_dir=list_dir+["interval"+str(x)]
     for list_dir in list_dir:
         dest_dir=os.path.join(postPro_destination,list_dir)
-        copytree(list_dir,postPro_destination)
+        bc.copytree(list_dir,postPro_destination)
     print("ready for postProcessing ...")
 
 def computePressureDropFoam(folder_name, sweep_name):
@@ -126,7 +64,7 @@ def computePressureDropFoam(folder_name, sweep_name):
         for line in lines:
             if "pressureDrop" in line:
                 os.chdir(basepath+folder_name)
-                with open("pressureDropvalues.txt","w") as mapression:
+                with open("pressureDropvalues.txt","a") as mapression:
                     mapression.write(line)
                     print(line)
                 mapression.close()
@@ -134,94 +72,13 @@ def computePressureDropFoam(folder_name, sweep_name):
     print("Done.\n\n")
     return(result)
 
-def prepareMyNextSweep(k):
-    sweep_name=mysweep.format(k+1)
-    previous_sweep_name=mysweep.format(k)
-    sweep_path=os.path.join(folder_name,sweep_name)
-    print("Starting shooting of "+sweep_name+"\n") 
-    # Copy Directories that were already shoot. Warning : put that after the computations
-    for x in range(1,k+1):
-        interval_name=myinterval.format(x)
-        source_interval=basepath+folder_name+"/"+previous_sweep_name+"/"+interval_name
-        destination_interval=basepath+folder_name+"/"+sweep_name
-        shutil.copytree(source_interval,os.path.join(destination_interval,os.path.basename(source_interval)))
-    #pimpleDyMFoam(folder_name, sweep_name, interval_name)
-    #preparePostProcessing(folder_name, sweep_name)
-    #computePressureDropFoam(folder_name, sweep_name)    
-    
-    #Preparing shooting directories from sweep1 data 
-    for i in range(k+1,n+1): #will become k+1, n+1 because of first loop being put into the big loop
-        interval_name=myinterval.format(i)
-        destination_constant=basepath+folder_name+"/"+sweep_name+"/"+interval_name
-        destination_system=basepath+folder_name+"/"+sweep_name+"/"+interval_name
-        previous_interval_name=myinterval.format(i-1)
-        #startTime=decimal_analysis(theta+deltaT*(i-2))
-        endTime=decimal_analysis(theta+deltaT*(i-1))
-        
-        source_constant=basepath+folder_name+"/"+previous_sweep_name+"/"+interval_name+'/constant'
-        source_system=basepath+folder_name+"/"+previous_sweep_name+"/"+interval_name+'/system'
-        source_endTime=basepath+folder_name+"/"+previous_sweep_name+"/"+previous_interval_name+'/'+str(endTime)        
-        destination_endTime=basepath+folder_name+"/"+sweep_name+"/"+interval_name        
-        
-        shutil.copytree(source_constant,os.path.join(destination_constant,os.path.basename(source_constant)))
-        shutil.copytree(source_system,os.path.join(destination_system,os.path.basename(source_system)))
-        shutil.copytree(source_endTime,os.path.join(destination_endTime,os.path.basename(source_endTime)))
-        
-        print("Computing for: \n"+sweep_name+"\n"+interval_name+"\n"+"Previous end time, that is current start time: "+str(endTime))
 
 
-def sweep_1_initialization(n):
-#Fetch all the files from src directories and modify them for the specific case : constant, system, start_time_dir, polyMesh, controlDict
-    k=1
-    os.mkdir(folder_name)
-    print("\nThe directory "+folder_name+" has been created at this place: \n"+basepath+"\n\n")
-    sweep_name=mysweep.format(k)
-    sweep_path=os.path.join(folder_name,sweep_name)
-    os.mkdir(sweep_path)
-    for i in range(1,n+1):
-        interval_name=myinterval.format(i)
-            
-        # Fetching Directory constant
-        source_constant=calcs_undeformed+'constant'
-        destination_constant=basepath+folder_name+"/"+sweep_name+"/"+interval_name
-        shutil.copytree(source_constant,os.path.join(destination_constant,os.path.basename(source_constant)))
-        
-        # Fetching Directory system
-        source_system=calcs_undeformed+'system'
-        destination_system=basepath+folder_name+"/"+sweep_name+"/"+interval_name
-        shutil.copytree(source_system,os.path.join(destination_system,os.path.basename(source_system)))
-        
-        # Fetching Directory with starting time
-        startTime=decimal_analysis(theta+deltaT*(i-1))
-        endTime=decimal_analysis(theta+deltaT*i)
-        source_startTime=calcs_undeformed+str(startTime)
-        destination_startTime=basepath+folder_name+"/"+sweep_name+"/"+interval_name
-        shutil.copytree(source_startTime,os.path.join(destination_startTime,os.path.basename(source_startTime)))
-        
-        # Deleting wrong polyMesh/points in the starting time directory
-        polyMesh_path=basepath+folder_name+"/"+sweep_name+"/"+interval_name+'/constant/polyMesh/points'
-        print('The file '+folder_name+'/'+sweep_name+'/'+interval_name+'/constant/polyMesh/points has been succefully removed.\n\n')
-        os.remove(polyMesh_path)
-        
-        #Fetching the right polyMesh
-        source_newPolyMesh=ref_cases_mod_def+"constant/polyMesh/points"
-        destination_newPolyMesh=basepath+folder_name+"/"+sweep_name+"/"+interval_name+'/constant/polyMesh'
-        shutil.copy(source_newPolyMesh,destination_newPolyMesh)
-
-        #Modify the controlDict file to ajust startTime and endTime        
-        controlDict_path=folder_name+'/'+sweep_name+'/'+interval_name+'/system/controlDict'
-        for line in fileinput.input(controlDict_path, inplace=True):
-            if line.startswith('startTime'):
-                line = 'startTime       {};\n'.format(startTime)
-            elif line.startswith('endTime'):
-                line = 'endTime         {};\n'.format(endTime)
-            print(line)
-    return("Sweep 1 initialization is done.")
 
 ################################        INITIALIZATION      ################################
 #folder_name=input("Name your folder: ")
 
-folder_name="test_intervals"
+folder_name="new_intervals"
 n=4;
 theta=0.4;
 T=0.1;
@@ -234,7 +91,7 @@ mysweep="sweep{}"
 
 ################################        SWEEP 1 INITIALIZATION      ################################
 
-sweep_1_initialization(n)
+bc.sweep_1_initialization(n, folder_name, basepath, calcs_undeformed, theta, deltaT, ref_cases_mod_def)
 
 ################################        COMPUTE MY SWEEP : sweep k in [1-n]       ################################
 for k in range(1,n+1):
@@ -246,4 +103,4 @@ for k in range(1,n+1):
         pimpleDyMFoam(folder_name, sweep_name, i)
     preparePostProcessing(folder_name, sweep_name)
     computePressureDropFoam(folder_name, sweep_name)    
-    prepareMyNextSweep(k)
+    pre.prepareMyNextSweep(k, n, folder_name, basepath, theta, deltaT)
