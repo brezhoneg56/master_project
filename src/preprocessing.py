6ef4b84a361984d60789eaacc4bce9a28fa4d64b
brezhoneg56 <julien.cosson56@gmail.com>
2023-05-03 15:29:10 +0200

    time_dest

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -121,13 +121,13 @@ def prepareLinearization(folder_name, sweep_name, interval_name, i): ##WORKS
     #starttime_source=primitive_path+folder_name+"/"+sweep_name+"/"+interval_name+"/"+str(bc.decimal_analysis(theta+(i-1)*deltaT))+"/"
     starttime_dest=steffensen_path+folder_name+"/"+sweep_name+"/"+interval_name+"/"+str(bc.decimal_analysis(theta+(i-1)*deltaT))+"/"
 
-    time_dest=steffensen_path+folder_name+"/"+sweep_name+"/"+interval_name+"/"
     compteur_debut=(i-1)*deltaT+t
     compteur_fin=i*deltaT
     for l in range(int(compteur_debut*1000),int(compteur_fin*1000)+1,int(t*1000)):
         pas=l/1000
         #print("\n\n timesource="+str(time_source/1000)+" and l="+str(l/1000))
         time_source=primitive_path+folder_name+"/"+sweep_name+"/"+interval_name+"/"+str(bc.decimal_analysis(theta+pas))
+        time_dest=steffensen_path+folder_name+"/"+sweep_name+"/"+interval_name+"/"+str(bc.decimal_analysis(theta+pas))
         shutil.copytree(time_source, time_dest)
     
     shutil.copy(linP_path, starttime_dest)
