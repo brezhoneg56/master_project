a2f86c488f0d708f5bfdfef6dc53b08928a1e824
brezhoneg56 <julien.cosson56@gmail.com>
2023-05-04 10:40:31 +0200

    typo

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -66,7 +66,7 @@ def prepareMyNextSweep(k, folder_name):
 def prepareShootingUpdate(folder_name, sweep_name, interval_name, k, i):#should start from sweep2, after interval2 is done
     #Copy Violet, Red, Blue and Green to prepare yellow (cf model)
     print('Preparing Shooting Update for '+interval_name+".\n")
-    next_sweep=mysweep.format(k)
+    next_sweep=mysweep.format(k+1)
     next_interval=myinterval.format(i+1)
     if not os.path.exists(steffensen_path+folder_name+"/"+sweep_name+"/preProcessing/"):
         shootingUpdate=os.mkdir(steffensen_path+folder_name+"/"+sweep_name+"/preProcessing/")
@@ -94,7 +94,7 @@ def prepareShootingUpdate(folder_name, sweep_name, interval_name, k, i):#should 
     shutil.copy(src_red_folder+"linUf", zero_shootingUpdate+"dUduf")
     print("Copy code GREEN done.\n")
     print("Copy code GREEN.\n")
-    src_green_folder=steffensen_path+folder_name+"/"+next_sweep+"/"+interval_name+"/"+str(bc.decimal_analysis(theta(i-1)*deltaT))+"/"
+    src_green_folder=steffensen_path+folder_name+"/"+next_sweep+"/"+interval_name+"/"+str(bc.decimal_analysis(theta+(i-1)*deltaT))+"/"
     shutil.copy(src_green_folder+"p", zero_shootingUpdate+"shootingUpdateP_left")
     shutil.copy(src_green_folder+"U", zero_shootingUpdate+"shootingUpdateU_left")
     shutil.copy(src_green_folder+"Uf", zero_shootingUpdate+"shootingUpdateUf_left")
