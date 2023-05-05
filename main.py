11ba7a1f7708ab6723b0571feb286b868452fb3b
brezhoneg56 <julien.cosson56@gmail.com>
2023-05-05 10:13:05 +0200

    shooting

--- main.py
+++ main.py
@@ -17,32 +17,34 @@ os.chdir(basepath)
 ################################        COMPUTE MY SWEEPs : sweep k in [1-n]       ################################
 #AUTOMATIC FILE DELETION/CREATIION
 folder_name="5_intervals"
-if os.path.exists(folder_name):
-    shutil.rmtree(folder_name)
-    print("Previous directory named "+folder_name+" has been replaced.")
 
+####uncomment################################################################################################
+#if os.path.exists(folder_name):
+#    shutil.rmtree(folder_name)
+#    print("Previous directory named "+folder_name+" has been replaced.")
+############################################################################################################
 #Primitive shooting : WOrks
 #sol.primal_nofastpropagator_seq()
 #Steffensen shooting : ongoing
 os.chdir(steffensen_path)
 #sol.computeSteffensenMethod(folder_name)
 
-#############     FUNCTION     #####################################################################
+#############     FUNCTION     #################################################################################################
 #Initialisation of Sweep 1  
-sweep_name="sweep1"
-pre.initializeLinearisation(folder_name, sweep_name) ##WORKS
+#sweep_name="sweep1"
+#pre.initializeLinearisation(folder_name, sweep_name) ##WORKS
 #for k in range (1, n+1):
 k=1
 sweep_name=mysweep.format(k)
-print("\n\nStarting linearisation process for "+sweep_name+".\n")
-for i in range (1, n+1):
-    interval_name=myinterval.format(i)  
-    print("\nlinearisedPimpleDyMFoam\n" )
-    sol.linearisedPimpleDyMFoam(folder_name, sweep_name, i)
-    print("Done\n")
-    print("prepareNextLinearization\n")
-    pre.prepareNextLinearization(folder_name, k, i)
-    print("Linearisation Preparation Done\n")
+#print("\n\nStarting linearisation process for "+sweep_name+".\n")
+#for i in range (1, n+1):
+#    interval_name=myinterval.format(i)  
+#    print("\nlinearisedPimpleDyMFoam\n" )
+#    sol.linearisedPimpleDyMFoam(folder_name, sweep_name, i)
+#    print("Done\n")
+#    print("prepareNextLinearization\n")
+#    pre.prepareNextLinearization(folder_name, k, i)
+#    print("Linearisation Preparation Done\n")
 
 
 i=2
@@ -60,6 +62,8 @@ interval_name=myinterval.format(i)
 sol.computeShootingUpdate(folder_name, sweep_name, interval_name)
 post.shootingUpdateP(folder_name, sweep_name, interval_name, k, i)
 print("Shooting Updated.\n")
+
+######################################################################################################################################
 
 #for i in range (1, n+1):
 #    interval_name=myinterval.format(i)
