9e35a26b7af0ba64750574a7fc05b95e51ac3c31
brezhoneg56 <julien.cosson56@gmail.com>
2023-05-12 10:26:02 +0200

    test n15

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ basepath=primitive_path
 # After testing is done, please uncomment the following
 #n=int(input("Set the number of shooting intervals: "));
 #theta=input("Define the starting time (example: 0.4): ");
-n=4; #Amount of sweeps / shooting intervals
-folder_name=str(n)+"_intervals_v2levrai"
+n=15; #Amount of sweeps / shooting intervals
+folder_name=str(n)+"_intervals"
 theta=0.4; #Starting time in seconds
 T=0.1; #Length of one period
 a=n; #Amount of sweeps in the first loop
